3187db54234e05a9a2bd228987c1f454f98243db
CodeDeX <splashdexstudios@gmail.com>
2026-08-15 17:05:08 +0000

    [fix] added KMP android library to root build script

--- DeX/build.gradle.kts
+++ DeX/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
   alias(libs.plugins.kotlin.serialization) apply false
   alias(libs.plugins.kotlin.multiplatform) apply false
   alias(libs.plugins.compose.multiplatform) apply false
+  alias(libs.plugins.android.multiplatform.library) apply false
 }
